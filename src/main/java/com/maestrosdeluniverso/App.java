4d1c4e828ccc22201c931910023f86ea6821be0c
hiisma <isma.ao.inf@gmail.com>
2022-03-08 17:51:22 +0100

    Commit Inicial

---
+++ src/main/java/com/maestrosdeluniverso/App.java
@@ -0,0 +1,13 @@
+package com.maestrosdeluniverso;
+
+/**
+ * Carrera de camells
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
