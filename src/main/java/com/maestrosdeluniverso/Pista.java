cf3c92d315c112cdbf7e95b762980160f243d267
hiisma <isma.ao.inf@gmail.com>
2022-03-11 12:13:08 +0100

    comentaris fets amb javadocs

--- src/main/java/com/maestrosdeluniverso/Pista.java
+++ src/main/java/com/maestrosdeluniverso/Pista.java
@@ -7,25 +7,47 @@ import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.graphics.GLine;
 
-// Defineix la pista de la carrera
+/**
+ * Defineix la pista de la carrera.
+ */
 public class Pista {
-    // Posició de la pantalla on començarà la carrera
+    /**
+     * Posició de la pantalla on començarà la carrera.
+     */
     static final int PosicioInicial = 10;
-    // Separació entre camells
+    /**
+     * Separació entre camells.
+     */
     static final int separacio = 70;
-    // Temps per començar la carrera
+    /**
+     * Temps per començar la carrera.
+     */
     static final int tempsInici = 2000;
-    // Temps d'espera entre passades
+    /**
+     * Temps d'espera entre passades.
+     */
     static final int tempsPausa = 80;
-    // Lloc en el que s'ha de pintar
+    /**
+     * Lloc en el que s'ha de pintar.
+     */
     private Carrera pantalla = null;
-    // Camells que participen en la carrera
+    /**
+     * Camells que participen en la carrera.
+     */
     private ArrayList<Camell> camells = null;
-    // Lloc en el que està la línia d'arribada de la pista
+    /**
+     * Lloc en el que està la línia d'arribada de la pista.
+     */
     private double liniaFinal = 0;
-    // Estrella que segueix el que va primer
+    /**
+     * Estrella que segueix el que va primer.
+     */
     private GImage estrella = null;
-    // Crear una pista de la mida especificada
+    /**
+     * Crear una pista de la mida especificada
+     * @param c carrera.
+     * @param mida mida de la pista.
+     */
     public Pista(final Carrera c, final double mida){
         pantalla = c;
         camells = new ArrayList<Camell>();
