f147f5babde32af1436784e209edda532269d667
hiisma <isma.ao.inf@gmail.com>
2022-03-18 15:33:46 +0100

    Herencia Graphics Program

---
+++ src/main/java/com/maestrosdeluniverso/ProvaGrafics.java
@@ -0,0 +1,12 @@
+package com.maestrosdeluniverso;
+
+import acm.graphics.GImage;
+import acm.program.Program;*;
+
+public class ProvaGrafics {
+    public static void executar() {
+        GImage imatge = new GImage("C:\\Users\\ismat\\Pictures\\Saved Pictures\\Horari.png");
+        acm.program
+    }
+    
+}
